9854f9498583d97615b13061d4b0b885f0bddbfe
Sarthakhanna0300 <sarthaksk6@gmail.com>
2021-02-03 23:12:43 +0530

    initial

---
+++ cmake-build-debug/CMakeFiles/CP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CP.dir/main.cpp.obj"
+  "CP.exe"
+  "CP.exe.manifest"
+  "CP.pdb"
+  "libCP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
